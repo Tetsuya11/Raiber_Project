07a29dc14638f3704dfa3fdc4eaabf74be448d01
taku845 <brn0612@gmail.com>
2015-06-08 15:37:57 +0800

    PostControllerの認証機能を記述しました

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,10 +1,23 @@
 <?php
 
+App::uses('AppController', 'Controller');
+
 class PostsController extends AppController{
 		var $name = "Posts";
 		var $name = array("Post");
 		function index(){
-			$this->set("posts",$this->Post->findAll(null,null,))
+			$this->set("posts",$this->Post->findAll(null,null,));
 		}
 
+		public function beforeFilter() {
+			$this->Auth->allow('index');
+		}
+
+		public function index() {
+			if ($this->Auth->loggedIn()) {
+				$this->Session->setFlash(_('ログインしています。'));
+			} else {
+				$this->Session->setFlash(_('ログインしていません。'));
+			}
+		}
 }
