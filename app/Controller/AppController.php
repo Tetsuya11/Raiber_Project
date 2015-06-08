1cf7d72e47c2ca57f72710a4e7ba8e67f9658b17
taku845 <brn0612@gmail.com>
2015-06-08 14:38:36 +0800

    memberのmodelとapp controllerをいじりました

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,4 +31,13 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+	public $helpers = array('Form', 'Html');
+	var $components = array(
+		'DebugKit.Toolbar',
+		'Auth' => array(
+			'loginRedirect' => array('controller' => '', 'action' => 'index'),
+			'logoutRedirect' => array('controller' => '', 'action' => 'index'),
+		),
+		'Session',
+	);
 }
