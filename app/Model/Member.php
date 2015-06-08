1613c3154e6bf1ac20e49f297991ec424a1b511e
taku845 <brn0612@gmail.com>
2015-06-08 15:11:55 +0800

    member.php改変（先生の承認済みです）

--- app/Model/Member.php
+++ app/Model/Member.php
@@ -5,11 +5,6 @@ class Member extends AppModel {
 	public $displayField = 'name';
 
 	public $validate = array(
-		'id' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-			),
-		),
 		'name' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
@@ -18,38 +13,7 @@ class Member extends AppModel {
 		'password' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
-			),
-		),
-		'type' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-			),
-		),
-		'fb_id' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-			),
-		),
-		'picture' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-			),
-		),
-		'created' => array(
-			'datetime' => array(
-				'rule' => array('datetime'),
-			),
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-			),
-		),
-		'modefied' => array(
-			'datetime' => array(
-				'rule' => array('datetime'),
-			),
-		    'notEmpty' => array(
-		    	'rule' => array('notEmpty'),
-		    ),
+			)
 		),
 	);
 }
