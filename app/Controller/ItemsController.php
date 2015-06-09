107172caa9a01daa643ae515d1cf6da0eeb9f4eb
sho33y <kidsdream226@gmail.com>
2015-06-09 13:21:39 +0800

    item ２回目

--- app/Controller/ItemsController.php
+++ app/Controller/ItemsController.php
@@ -4,10 +4,25 @@ class ItemsController extends AppController{
 
 		public $helpers = array('Html','Form');
 
+
 		public function index() {
         $this->set('items', $this->Item->find('all'));
-        
-    }
+
+    	}
+
+
+        public function view($id = null) {
+	        if (!$id) {
+	            throw new NotFoundException(__('Invalid post'));
+	        }
+
+	        $item = $this->Item->findById($id);
+	        if (!$item) {
+	            throw new NotFoundException(__('Invalid post'));
+	        }
+	        $this->set('item', $item);
+	    }
+
 
 
 }
