1d93da51524dabc137f3a377485e5bdfe9231a18
taku845 <brn0612@gmail.com>
2015-06-10 13:15:36 +0800

    user.sql

--- app/View/Users/add.ctp
+++ app/View/Users/add.ctp
@@ -5,9 +5,9 @@
         <legend><?php echo __('Sign Up'); ?></legend>
         <?php echo $this->Form->input('name');
         echo $this->Form->input('password');
-        //echo $this->Form->input('role', array(
-            //'options' => array('admin' => 'Admin', 'author' => 'Author')
-        //)
+        echo $this->Form->input('role', array(
+            'options' => array('admin' => 'Admin', 'author' => 'Author')
+        )
 		);
     ?>
     </fieldset>
--- users.sql
+++ users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10.9
+-- version 4.4.6
 -- http://www.phpmyadmin.net
 --
--- ホスト: localhost
--- 生成日時: 2015 年 6 月 09 日 12:12
--- サーバのバージョン: 5.1.73
--- PHP のバージョン: 5.3.3
+-- Host: localhost
+-- Generation Time: 2015 年 6 月 10 日 05:09
+-- サーバのバージョン： 5.6.24
+-- PHP Version: 5.5.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- データベース: `raiber_project`
+-- Database: `Raiber_Project`
 --
 
 -- --------------------------------------------------------
@@ -27,16 +27,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `password` varchar(100) NOT NULL,
   `type` varchar(1) NOT NULL,
   `fb_id` int(11) DEFAULT NULL,
   `picture` varchar(255) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `users`
@@ -44,8 +43,28 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `name`, `password`, `type`, `fb_id`, `picture`, `created`, `modified`) VALUES
 (1, 'Sho', 'yottyan', 'U', NULL, 'images.jpeg', '2015-05-03 00:00:00', '2015-06-05 00:00:00'),
-(2, 'TAkuya', 'takuya', 'U', NULL, 'images (1).jpeg', '2015-04-20 00:00:00', '2015-06-05 00:00:00');
+(2, 'TAkuya', 'takuya', 'U', NULL, 'images (1).jpeg', '2015-04-20 00:00:00', '2015-06-05 00:00:00'),
+(3, 'John', '$2a$10$a2cMnxBTFixB6u1E25pCPubwFeK/lDyOSLLBM8UrmNjPh8VsLSbnK', '', NULL, NULL, '2015-06-09 22:49:41', '2015-06-09 22:49:41');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
