2dc1ac14804979aa10a4346b02ec994b66c2dfa0
taku845 <brn0612@gmail.com>
2015-06-23 03:42:14 +0800

    init commit

--- app/Model/User.php
+++ app/Model/User.php
@@ -3,6 +3,8 @@ App::uses('AppModel', 'Model');
 App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 
 class User extends AppModel {
+
+    public $hasMany = 'item';
     
     public $hasMany = 'Post';
 
