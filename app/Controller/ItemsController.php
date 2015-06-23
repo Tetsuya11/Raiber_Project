a84d2291e86aca3ea945906338e7d6692798539a
sho33y <kidsdream226@gmail.com>
2015-06-23 14:10:24 +0800

    no message

--- app/Controller/ItemsController.php
+++ app/Controller/ItemsController.php
@@ -47,6 +47,9 @@ class ItemsController extends AppController{
 
 	        if ($this->request->is('post')) {
 		            $this->Post->create();
+
+		            $this->request->data['Item']['user_id'] = $this->Auth->user('id');
+
 		            if ($this->Post->save($this->request->data)) {
 		                $this->Session->setFlash(__('投稿完了！ Your post has been saved.'));
 		                return $this->redirect(array('action' => 'view',$id));
